7cfbe7377952bcd43381014cf3491157b38bc4df
Leonardo Alves <leonardoalves29@hotmail.com>
2025-07-14 18:47:24 -0300

    Update Projeto Automaçao rascunho.py

--- Projeto_Final_Automação_Python/Projeto Automaçao rascunho.py
+++ Projeto_Final_Automação_Python/Projeto Automaçao rascunho.py
@@ -2,7 +2,7 @@ import pyautogui
 import time 
 
 
-time.sleep(5)
-print(pyautogui.position())
+time.sleep(5) #Coloquei um Delay de 5 segundos para dar tempo de colocar o ponteiro do mouse no lugar desejado
+print(pyautogui.position())  #Aqui é pra achar a posição do mouse
 pyautogui.press("Home")
 
